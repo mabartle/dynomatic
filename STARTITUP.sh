c32cc3a08b8678bad40c6ff344ed8cb546fccae0
Mark Bartola <mabartle@gmail.com>
2017-06-21 10:04:24 -0400

    Adding notes to STARTITUP script for variable setting

--- STARTITUP.sh
+++ STARTITUP.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Start up script to kick off the DynamoDB(LOCAL) 
+# Install DynamoDB LOCALLY and change the directory path below to match where
+# it is installed
 dynamofiledir=/opt/DYNAMO/
+# Set the path below to match where the aws config file resides
 awsconfigfilepath=/home/markb/.aws/config
 echo 'Starting the DynamoDB Instance LOCALLY'
 echo 'Using AWS Config file here:' $awsconfigfilepath
